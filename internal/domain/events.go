a944a3c216e840cc409b7938644f7d08647784a0
Giovanni <giovannirco@gmail.com>
2026-04-04 13:14:11 -0300

    refactor(domain): remove unused CalendarEvent and EventCalendarResult types
    V2 events calendar uses EventsCalendarResult from upstream.go.
    These types were added but never referenced by any handler or fetch function.

--- internal/domain/events.go
+++ internal/domain/events.go
@@ -15,24 +15,3 @@ type EventsResult struct {
 	AllEvents  []string   `json:"all_events"`
 }
 
-type CalendarEvent struct {
-	ID                 int      `json:"id"`
-	Name               string   `json:"name"`
-	Description        string   `json:"description"`
-	ColorDark          string   `json:"color_dark"`
-	ColorLight         string   `json:"color_light"`
-	DisplayPriority    int      `json:"display_priority"`
-	SpecialEffect      string   `json:"special_effect"`
-	StartDate          string   `json:"start_date"`
-	EndDate            string   `json:"end_date"`
-	IsRecurring        bool     `json:"is_recurring"`
-	RecurringWeekdays  []int    `json:"recurring_weekdays,omitempty"`
-	RecurringMonthDays []int    `json:"recurring_month_days,omitempty"`
-	RecurringStart     string   `json:"recurring_start,omitempty"`
-	RecurringEnd       string   `json:"recurring_end,omitempty"`
-	Tags               []string `json:"tags,omitempty"`
-}
-
-type EventCalendarResult struct {
-	Events []CalendarEvent `json:"events"`
-}
